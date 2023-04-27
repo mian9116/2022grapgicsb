f9e99d047abb422ee966b65939f22948d8dd6baf
Yuan Yuan <91170703+mian9116@users.noreply.github.com>
2023-04-27 16:24:16 +0800

    Add files via upload

---
+++ week11/week11-3_playsound.cpp
@@ -0,0 +1,14 @@
+#include <windows.h>
+#include <stdio.h>
+
+
+int main()
+{
+    ///PlaySound("C:\\Users\\Administrator\\Desktop\\Ado-re-mi\\do.wav",NULL,SND_ASYNC);
+    ///PlaySound("C:\\Users\\Administrator\\Desktop\\do-re-mi\\re.wav",NULL,SND_ASYNC);
+    ///PlaySound("C:\\Users\\Administrator\\Desktop\\do-re-mi\\mi.wav",NULL,SND_ASYNC);
+
+    PlaySound("do-re-mi\\do.wav",NULL,SND_ASYNC);
+
+    printf("Hello World");
+}
